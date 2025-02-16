33dc9b83e9971d2a10e4f27f5c78e2445c861a14
BugHunter-X <askzenai@gmail.com>
2025-02-16 21:52:05 +0100

    Add deepLinkBug.js file

---
+++ deepLinkBug.js
@@ -0,0 +1,1 @@
+This bug occurs when using the Expo `Linking` API to handle deep links.  The `Linking.addEventListener` method might not always trigger when a deep link is opened, especially if the app is already running in the background. This leads to the app not properly processing the incoming link data.
